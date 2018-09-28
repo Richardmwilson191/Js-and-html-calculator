996f91374003387e542ee93952649379babb29db
Richie191 <richardmwilson191@gmail.com>
2018-09-27 23:35:22 -0500

    UI created

---
+++ script.js
@@ -0,0 +1,13 @@
+function addTo() {
+  let calculate = document.getElementById("one");
+  let calculate = document.getElementById("two");
+  let calculate = document.getElementById("three");
+  let calculate = document.getElementById("four");
+  let calculate = document.getElementById("five");
+  let calculate = document.getElementById("six");
+  let calculate = document.getElementById("seven").innerHTML;
+  let calculate = document.getElementById("eight");
+  let calculate = document.getElementById("nine");
+
+  document.getElementById("calculator_display").innerHTML = calculate;
+}
