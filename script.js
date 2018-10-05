27f594957d297a27dc3f67b3fdcb9913ca8c0875
Richie191 <richardmwilson191@gmail.com>
2018-10-05 17:01:18 -0500

    Calculates only single operator calculations

--- script.js
+++ script.js
@@ -1,31 +1,62 @@
-var calculate = [];
+let calculate = [];
+let answer = 0;
+
+//Function that clears the calculator display (the C button).
+
+function cls() {
+  console.log(calculate)
+  calculate = [];
+  document.getElementById("calculator_display").innerHTML = calculate.join("");
+}
 
 // Function that adds each elements to an array then displays it on the
 // calculator screen.
 
 function addTo(objButton) {
-  calculate.push(objButton.innerHTML);
-  document.getElementById("calculator_display").innerHTML = calculate.join("");
+  let num = objButton.innerHTML;
+  if (answer != 0 && (num == "x" || num == "+" || num == "/" || num == "-")) {
+    calculate = [];
+    calculate.push(answer);
+  }
+  else {
+    calculate.push(num);
+    document.getElementById("calculator_display").innerHTML = calculate.join("");
+  }
 }
 
+// Function that performs the calculations once the equal sign is pressed.
+
 function equal() {
-  let answer = 0;
+  let operator = [];
+  let numbers = [[],[]];
 
   for (let i = 0; i < calculate.length; i++) {
     const element = calculate[i];
-    
-    if (element != "x" || element != "+" || element != "/" || element != "-") {
-      let numbers = [[]];
-      numbers[[operator.length]].push(element);
-      if (operator.length == 2) {
-        let number_1 = Number(numbers[1][0].join(""));
-        let number_2 = Number(numbers[1][1].join(""));
-      }
-      
-    } else if (element == "x" || element == "+" || element == "/" || element == "-") {
-      let operator = [];
+    if (element == "x" || element == "+" || element == "/" || element == "-") {   
       operator.push(element);
     }
-    
+    else {
+      
+      numbers[operator.length].push(element);
+      console.log(numbers)
+      if (operator.length >= 1) {
+        let number_1 = Number(numbers[0].join(""));
+        let number_2 = Number(numbers[1].join(""));
+        switch (operator.pop()) {
+          case 'x':
+            answer = number_1 * number_2;
+            break;
+          case '+':
+            answer = number_1 + number_2;
+            break;
+          case '/':
+            answer = number_1 / number_2;
+            break;       
+          case '-':
+            answer = number_1 - number_2;
+        }
+      }
+    } 
   }
+  document.getElementById("calculator_display").innerHTML = answer;
 }
